3120cd3727be7e7f6f60070af8a77d622397e155
Paul <pd21248@gmail.com>
2023-12-03 15:15:51 -0800

    my initial commit for this project

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,23 @@
+import noproject from "../assets/no-projects.png";
+import Button from "./Button";
+
+export default function NoProjectSelected({onStartAddProject}) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        className="w-16 h-16 object-contain mx-auto"
+        src={noproject}
+        alt="An empty task list"
+      ></img>
+      <h2 className="text-xl font-bold text-stone-700 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-600 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p>
+        <Button onClick={onStartAddProject}>Create new project</Button>
+      </p>
+    </div>
+  );
+}
